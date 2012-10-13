77a8463d1c5abfbc297d87658fa3c827253a40d3
Matt <matt@mattsouth.net>
2012-10-13 20:51:36 +0100

    tidied up the code, fixed some problems with rough edges on the pieces

--- woodentrack.js
+++ woodentrack.js
@@ -20,8 +20,16 @@ function insideRadius() {
   return gridSize-trackWidth/2;
 }
 
-function curvePath(side, direction, radius, angle) {
-  return " a " + radius + ", " + radius + " 0 " + ((side==1) ? "0,1 " : "0,0 ") + direction*(Math.sin(angle)*radius) + ", " + (side*(1-Math.cos(angle))*radius)
+/*
+starting position facing along x axis...
+side = {1,-1} - 1: bend right, -1: bend left
+direction = {1, -1} - 1: forwards, -1: backwards
+orbit = {1,0} - 1: clockwise, 0: anti-clockwise
+radius = radius of curve (pixels)
+angle = angle of curve (radians)
+*/
+function curvePath(side, direction, orbit, radius, angle) {
+  return " a " + radius + ", " + radius + " 0 0, " + orbit + " " + direction*(Math.sin(angle)*radius) + ", " + (side*(1-Math.cos(angle))*radius)
 }
 
 // objects
@@ -62,10 +70,12 @@ function Straight(size) {
 }
 
 Straight.prototype.draw = function(svg) {
+  // draw track
   svg.append("path").attr("d", "M 0 0 v -" + trackWidth/2 + 
     " h " + this.size*gridSize + 
     " v " + trackWidth + 
     " h -" + this.size*gridSize + " z ");
+  // draw rails
   svg.append("path").attr("d", "M 0 -" + trackWidth/4 + 
     " h " + this.size*gridSize + 
     " m 0," + trackWidth/2 + 
@@ -78,15 +88,17 @@ function Bend(flip) {
 }
 
 Bend.prototype.draw = function(svg) {
+  // draw track
   svg.append("path").attr("d", "M 0 0 v " + (-1*this.flip*trackWidth/2) +
-    curvePath(this.flip, 1, gridSize+trackWidth/2, Math.PI/4) +
+    curvePath(this.flip, 1, (this.flip==1?1:0), gridSize+trackWidth/2, Math.PI/4) +
     " l -" + (sin45*trackWidth) + " " + (this.flip*sin45*trackWidth) +
-    curvePath(-1*this.flip, -1, gridSize-trackWidth/2, Math.PI/4) + 
+    curvePath(-1*this.flip, -1, (this.flip==1?0:1), gridSize-trackWidth/2, Math.PI/4) + 
     " z");
+  // draw rails
   svg.append("path").attr("d", "M 0 "+ (-1*this.flip*trackWidth/4) +
-    curvePath(this.flip, 1, gridSize+trackWidth/4, Math.PI/4) +
+    curvePath(this.flip, 1, (this.flip==1?1:0), gridSize+trackWidth/4, Math.PI/4) +
     " l -" + (sin45*trackWidth/2) + " " + (this.flip*sin45*trackWidth/2) +
-    curvePath(this.flip*-1, -1, gridSize-trackWidth/4, Math.PI/4));
+    curvePath(this.flip*-1, -1, (this.flip==1?0:1), gridSize-trackWidth/4, Math.PI/4));
   return svg.append("g").attr("transform", "translate(" + (sin45*gridSize) +"," + (this.flip*(1-sin45)*gridSize) +")").append("g").attr("transform","rotate(" + this.flip*45 + ")");
 }
 
@@ -96,19 +108,21 @@ function Junction(flip, exit) {
 }
 
 Junction.prototype.draw = function(svg) {
+  // draw track
   svg.append("path").attr("d", "M 0 0 v " + (-1*this.flip*trackWidth/2) +
     " h " + (2/3)*gridSize + 
     " v " + (this.flip*trackWidth) +
     " h -" + ((2/3)*gridSize-(Math.sqrt(2*trackWidth*gridSize))) +
     " a " + outsideRadius() + ", " + outsideRadius() + " 0 " + ((this.flip==1) ? "0,1 " : "0,0 ") + ((sin45*outsideRadius())-(Math.sqrt(2*gridSize*trackWidth))) + ", " + (this.flip*(insideRadius()-(sin45*outsideRadius()))) +
     " l -" + (sin45*trackWidth) + " " + (this.flip*sin45*trackWidth) +
-    curvePath(-1*this.flip, -1, gridSize-trackWidth/2, Math.PI/4) + 
+    curvePath(-1*this.flip, -1, (this.flip==1?0:1), gridSize-trackWidth/2, Math.PI/4) + 
     " z");
+  // draw rails
   svg.append("path").attr("d", "M 0 -"+trackWidth/4+" h " + 2*gridSize/3 + " m 0," + trackWidth/2 + " h -" + 2*gridSize/3);
   svg.append("path").attr("d", "M 0 "+(-1*this.flip*trackWidth/4)+
-    curvePath(this.flip, 1, gridSize+trackWidth/4, Math.PI/4) +
+    curvePath(this.flip, 1, (this.flip==1?1:0), gridSize+trackWidth/4, Math.PI/4) +
     " l -" + (sin45*trackWidth/2) + " " + (this.flip*sin45*trackWidth/2) +
-    curvePath(this.flip*-1, -1, gridSize-trackWidth/4, Math.PI/4)).attr("fill", "none");
+    curvePath(this.flip*-1, -1, (this.flip==1?0:1), gridSize-trackWidth/4, Math.PI/4)).attr("fill", "none");
   if (this.exit=="C") {
     return svg.append("g").attr("transform", "translate(" + (sin45*gridSize) +"," + (this.flip*(1-sin45)*gridSize) +")").append("g").attr("transform","rotate(" + this.flip*45 + ")");
   } else { // default exit is "B"
@@ -122,24 +136,28 @@ function Crossover(flip, exit) {
 }
 
 Crossover.prototype.draw = function(svg) {
+  // draw track
   var truncatedWidth = ((gridSize/(Math.cos(Math.PI/8)))-gridSize+(trackWidth/2));
   svg.append("path").attr("d", "M 0 0 v " + this.flip*trackWidth/2 +
-    curvePath(this.flip, 1, gridSize-(trackWidth/2), Math.PI/4) +
+    curvePath(this.flip, 1, (this.flip==1?1:0), gridSize-(trackWidth/2), Math.PI/4) +
     " l " + (sin45*truncatedWidth) + " " + (-1*this.flip*sin45*truncatedWidth) +
     " v " + -1*this.flip*truncatedWidth + 
     " a " + (gridSize-(trackWidth/2)) + ", " + (gridSize-(trackWidth/2)) + " 0, " + (this.flip==1?"0 1 ":"0 0 ") + (-1*sin45*(gridSize-(trackWidth/2))) + ", " + (-1*this.flip*(1-sin45)*(gridSize-(trackWidth/2))) +
     " l -" + (sin45*truncatedWidth) + " " + (this.flip*sin45*truncatedWidth) +
     " z");
+  // draw rails
   svg.append("path").attr("d", "M 0 " + this.flip*trackWidth/4 +
-    curvePath(this.flip, 1, gridSize-(trackWidth/4), Math.PI/4) +
+    curvePath(this.flip, 1, (this.flip==1?1:0), gridSize-(trackWidth/4), Math.PI/4) +
     " l " + (sin45*trackWidth/2) + " " + (-1*this.flip*sin45*trackWidth/2) +
-    curvePath(-1*this.flip, -1, gridSize+(trackWidth/4), Math.PI/4));
+    curvePath(-1*this.flip, -1, (this.flip==1?0:1), gridSize+(trackWidth/4), Math.PI/4) + " z");
   svg.append("path").attr("d", "M " + sin45*(truncatedWidth-3*trackWidth/4) + " " + -1*this.flip*((truncatedWidth-trackWidth/2)+(sin45*(truncatedWidth-3*trackWidth/4))) +
     " a " + (gridSize+(trackWidth/4)) + ", " + (gridSize+(trackWidth/4)) + " 0, " + (this.flip==1?"0 0 ":"0 1 ") + (sin45*(gridSize+(trackWidth/4))) + ", " + (this.flip*(1-sin45)*(gridSize+(trackWidth/4))) +
     " v " + -1*this.flip*trackWidth/2 +
-    " a " + (gridSize-(trackWidth/4)) + ", " + (gridSize-(trackWidth/4)) + " 0, " + (this.flip==1?"0 1 ":"0 0") + (-1*sin45*(gridSize-(trackWidth/4))) + ", " + (-1*this.flip*(1-sin45)*(gridSize-(trackWidth/4)))).attr("fill", "none");
+    " a " + (gridSize-(trackWidth/4)) + ", " + (gridSize-(trackWidth/4)) + " 0, " + (this.flip==1?"0 1 ":"0 0") + (-1*sin45*(gridSize-(trackWidth/4))) + ", " + (-1*this.flip*(1-sin45)*(gridSize-(trackWidth/4))) + " z").attr("fill", "none");
   if (this.exit=="C") {
-    return svg.append("g").attr("transform", "translate(" + 2*gridSize*Math.sin(Math.PI/8) + "," + this.flip * 2 *((gridSize/Math.cos(Math.PI/8))-gridSize)+ ")");
+    var x = gridSize*Math.sin(Math.PI/8);
+    var y = gridSize*Math.cos(Math.PI/8);
+    return svg.append("g").attr("transform", "translate(" + 2*x + "," + this.flip*2*(gridSize-y) + ")");
   } else { // default exit is "B"
     return svg.append("g").attr("transform", "translate(" + (sin45*gridSize) +"," + (this.flip*(1-sin45)*gridSize) +")").append("g").attr("transform","rotate(" + this.flip*45 + ")");
   }
